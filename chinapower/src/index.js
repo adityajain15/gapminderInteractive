0d7814342e7fcf3d89ce5880b42864de48c07ac9
Jacque Schrag <jnschrag@gmail.com>
2017-12-22 14:12:07 -0500

    Update basic version data and indicators

--- chinapower/src/index.js
+++ chinapower/src/index.js
@@ -1,12 +1,17 @@
 import * as plot from './js/createPlot'
-import Data from './data/20171218-basic.csv'
+import Data from './data/basic-data-20171222.csv'
+import Indicators from './data/basic-indicators-20171222.csv'
 import './scss/main.scss'
 import intro from 'intro.js'
 const introJs = intro.introJs()
 
-function init () {
-  plot.createPlot(Data)
-  introJs.start()
-}
+let plotted
 
+function init () {
+  plotted = plot.createPlot({
+    data: Data,
+    indicators: Indicators
+  })
+  plotted.init()
+}
 window.addEventListener('DOMContentLoaded', init)
