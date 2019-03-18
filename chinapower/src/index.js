9d6efeae5d3ccd115b6f17ae81ae071962df5da2
dlousa92 <dlousa92@gmail.com>
2019-03-18 16:25:36 -0400

    update data sheet references

--- chinapower/src/index.js
+++ chinapower/src/index.js
@@ -1,6 +1,6 @@
 import * as plot from './js/createPlot'
-import Data from './data/basic-data-20180205.csv'
-import Indicators from './data/basic-indicators-20171222.csv'
+import Data from './data/basic-data-2019315.csv'
+import Indicators from './data/basic-indicators-2019315.csv'
 import './scss/main.scss'
 import translations from './js/translations'
 import strings from './js/translationStrings'
