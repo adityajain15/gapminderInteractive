769389dd1b1de127c393d7cdd21739fe18a5140a
Jacque Schrag <jnschrag@gmail.com>
2020-01-23 11:18:11 -0500

    Add Air Pollution chart to ChinaPower

---
+++ chinapower/src/air-pollution.js
@@ -0,0 +1,21 @@
+import * as plot from "./js/createPlot";
+import Data from "./data/air-pollution-data-20200123.csv";
+import Indicators from "./data/air-pollution-indicators-20200123.csv";
+import "./scss/main.scss";
+import translations from "./js/translations";
+import strings from "./js/translationStrings";
+
+let plotted;
+let lang = translations.displayLang();
+const langStrings = strings.strings;
+
+function init() {
+  plotted = plot.createPlot({
+    data: Data,
+    indicators: Indicators,
+    language: lang,
+    strings: langStrings
+  });
+  plotted.init();
+}
+window.addEventListener("DOMContentLoaded", init);
--- chinapower/webpack.common.js
+++ chinapower/webpack.common.js
@@ -1,30 +1,31 @@
-const path = require('path')
-const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require("path");
+const CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
-    'index': './src/index.js',
-    'advanced': './src/advanced.js'
+    index: "./src/index.js",
+    advanced: "./src/advanced.js",
+    airPollution: "./src/air-pollution.js"
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(process.cwd(), 'dist')
+    filename: "[name].js",
+    path: path.resolve(process.cwd(), "dist")
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         query: {
-          presets: [ 'env', 'stage-2' ]
+          presets: ["env", "stage-2"]
         }
       },
       {
         test: /\.csv$/,
-        loader: 'csv-loader',
+        loader: "csv-loader",
         options: {
           dynamicTyping: true,
           header: true,
@@ -33,22 +34,22 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader'
+        loader: "file-loader"
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
+          fallback: "style-loader",
           use: [
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               query: {
                 modules: false,
-                localIdentName: '[name]__[local]___[hash:base64:5]'
+                localIdentName: "[name]__[local]___[hash:base64:5]"
               }
             },
-            'postcss-loader'
+            "postcss-loader"
           ]
         })
       },
@@ -56,18 +57,18 @@ module.exports = {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
+          fallback: "style-loader",
           use: [
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               query: {
                 modules: false,
                 sourceMap: true,
                 importLoaders: 2,
-                localIdentName: '[name]__[local]___[hash:base64:5]'
+                localIdentName: "[name]__[local]___[hash:base64:5]"
               }
             },
-            'sass-loader'
+            "sass-loader"
           ]
         })
       }
@@ -76,22 +77,28 @@ module.exports = {
 
   plugins: [
     new CommonsChunkPlugin({
-      name: 'commons',
-      filename: 'commons.js',
-      chunks: ['index', 'advanced']
+      name: "commons",
+      filename: "commons.js",
+      chunks: ["index", "advanced"]
     }),
-    new ExtractTextPlugin('style.css'),
+    new ExtractTextPlugin("style.css"),
     new HtmlWebpackPlugin({
-      inject: 'body',
-      chunks: ['commons', 'index'],
-      template: './src/index.html',
-      filename: 'index.html'
+      inject: "body",
+      chunks: ["commons", "index"],
+      template: "./src/index.html",
+      filename: "index.html"
     }),
     new HtmlWebpackPlugin({
-      inject: 'body',
-      chunks: ['commons', 'advanced'],
-      template: './src/advanced.html',
-      filename: 'advanced.html'
+      inject: "body",
+      chunks: ["commons", "advanced"],
+      template: "./src/advanced.html",
+      filename: "advanced.html"
+    }),
+    new HtmlWebpackPlugin({
+      inject: "body",
+      chunks: ["commons", "airPollution"],
+      template: "./src/air-pollution.html",
+      filename: "air-pollution.html"
     })
   ]
-}
+};
